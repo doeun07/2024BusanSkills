2058f7feef8508622642394db8076813f84878c3
최도은 <choidoeun0102@gmail.com>
2025-01-22 17:32:22 +0900

    api 라우터 설정

--- index.php
+++ index.php
@@ -7,32 +7,51 @@ $path[1] = isset($path[1]) ? $path[1] : null;
 $resource = explode("/", $path[0]);
 
 $page = "";
-switch ($resource[1]) {
-    case "":
-        $page = "./pages/index.php";
-        break;
-    case "information":
-        $page = "./pages/sub01.php";
-        break;
-    case "statistics":
-        $page = "./pages/sub02.php";
-        break;
-    case "reservation":
-        $page = "./pages/sub03.php";
-        break;
-    case "goods":
-        $page = "./pages/sub04.php";
-        break;
-    case "login":
-        $page = "./pages/login.php";
-        break;
-    case "register":
-        $page = "./pages/register.php";
-        break;
-    default:
-        echo "잘못된 접근입니다.";
-        return 0;
+if ($resource[1] == "api") {
+    switch ($resource[2]) {
+        case "register":
+            $page = "./api/register.php";
+            break;
+        case "login":
+            $page = "./api/login.php";
+            break;
+        default:
+            echo "잘못된 api 접근입니다.";
+            break;
+    }
+
+    include $page;
+} else {
+    switch ($resource[1]) {
+        case "":
+            $page = "./pages/index.php";
+            break;
+        case "information":
+            $page = "./pages/sub01.php";
+            break;
+        case "statistics":
+            $page = "./pages/sub02.php";
+            break;
+        case "reservation":
+            $page = "./pages/sub03.php";
+            break;
+        case "goods":
+            $page = "./pages/sub04.php";
+            break;
+        case "login":
+            $page = "./pages/login.php";
+            break;
+        case "logout":
+            $page = "./pages/logout.php";
+            break;
+        case "register":
+            $page = "./pages/register.php";
+            break;
+        default:
+            echo "잘못된 접근입니다.";
+            return 0;
+    }
+    include "./component/header.php";
+    include $page;
+    include "./component/footer.php";
 }
-include "./component/header.php";
-include $page;
-include "./component/footer.php";
